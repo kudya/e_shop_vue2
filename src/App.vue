4b983d0d1f835536168483057e70f2e54ff578c0
Anton Kurdin <anton.kurdin@gmail.com>
2021-07-09 13:39:55 +0300

    module/6.cart:  в проект добавлена и изучена библиотека vue-router, заменён самописный роутер на vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,9 @@
 <template>
- <component :is="currentPageComponent" :page-params="currentPageParams" />
+  <router-view></router-view>
 </template>
 
 <script>
-import MainPage from '@/pages/MainPage.vue';
-import ProductPage from '@/pages/ProductPage.vue';
-import NotFoundPage from '@/pages/NotFoundPage.vue';
-import eventBus from '@/eventBus';
-
-const routes = {
-  main: 'MainPage',
-  product: 'ProductPage',
-};
-
 export default {
-  components: {
-    MainPage,
-    ProductPage,
-    NotFoundPage,
-  },
-  data() {
-    return {
-      currentPage: 'main',
-      currentPageParams: {},
-    };
-  },
-  computed: {
-    currentPageComponent() {
-      return routes[this.currentPage] || 'NotFoundPage';
-    },
-  },
-  methods: {
-    goToPage(pageName, pageParams) {
-      this.currentPage = pageName;
-      this.currentPageParams = pageParams || {};
-    },
-  },
-  created() {
-    eventBus.$on('goToPage', (pageName, pageParams) => this.goToPage(pageName, pageParams));
-  },
+
 };
 </script>

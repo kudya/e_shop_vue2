691dc72629722889f6affeedcb4ceee6e6ee67e3
Anton Kurdin <anton.kurdin@gmail.com>
2021-07-09 19:44:12 +0300

    module/6.cart: добавлены геттеры внутри store и возможность их проксировать внутри страницы CartPage, доработана страница CartPage фунционалом динамического отображения заказанных товаров с их общей стоимостью

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import products from '@/data/products';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -11,13 +13,24 @@ export default new Vuex.Store({
   },
   mutations: {
     addProductToCart(state, { productId, amount }) {
-      const item = state.find((product) => product.productId === productId);
+      const item = state.cartProducts.find((product) => product.productId === productId);
 
       if (item) {
         item.amount += amount;
       } else {
         state.cartProducts.push({ productId, amount });
       }
+    },
+  },
+  getters: {
+    cartDetailProducts(state) {
+      return state.cartProducts.map((item) => ({
+        ...item,
+        product: products.find((p) => p.id === item.productId),
+      }));
+    },
+    cartTotalPrice(state, getters) {
+      return getters.cartDetailProducts.reduce((acc, item) => acc + (item.product.price * item.amount), 0);
     },
   },
 });

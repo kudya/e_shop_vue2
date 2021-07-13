dbd1aa8608a33fe4a4fa25b0c6fcb1b257a9c755
Anton Kurdin <anton.kurdin@gmail.com>
2021-07-13 15:16:08 +0300

    module/7.productList: в проект добавлена библиотека axios для создания запросов к API, функционал подгрузки всех товаров с пагинацией переписан на данных, получаемых через API вместо локальных данных

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -30,6 +30,7 @@ import products from '@/data/products';
 import ProductList from '@/components/ProductList.vue';
 import BasePagination from '@/components/BasePagination.vue';
 import ProductFilter from '@/components/ProductFilter.vue';
+import axios from 'axios';
 
 export default {
   components: {
@@ -43,6 +44,7 @@ export default {
       filterColor: null,
       page: 1,
       productsPerPage: 3,
+      productsData: null,
     };
   },
   computed: {
@@ -68,12 +70,28 @@ export default {
       return filteredProducts;
     },
     products() {
-      const offset = (this.page - 1) * this.productsPerPage;
-      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
+      return this.productsData ? this.productsData.items.map((product) => ({
+        ...product,
+        image: product.image.file.url,
+      })) : [];
     },
     countProducts() {
-      return this.filteredProducts.length;
+      return this.productsData ? this.productsData.pagination.total : 0;
     },
+  },
+  methods: {
+    loadProducts() {
+      axios.get(`https://vue-study.skillbox.cc/api/products?page=${this.page}&limit=${this.productsPerPage}`)
+        .then((response) => { this.productsData = response.data; });
+    },
+  },
+  watch: {
+    page() {
+      return this.loadProducts();
+    },
+  },
+  created() {
+    this.loadProducts();
   },
 };
 </script>
